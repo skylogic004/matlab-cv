6fe03e277156bacb2935938af988c142ad915a90
IssamLaradji <issam.laradji@gmail.com>
2014-12-03 14:15:43 -0800

    improved matLearn_CV

--- matLearn_CV.m
+++ matLearn_CV.m
@@ -26,22 +26,25 @@ function [bestModel] = matLearn_CV(X, y, options)
         - Issam Laradji, Matt Dirks (2014)
     %}
     % Default values
-    options.model
-    [model, nFolds, paramName, paramValues, lossFunction, shuffle] ...
+    [model, nFolds, paramName, paramValues, loss, shuffle] ...
            = myProcessOptions(options, 'model', NaN, 'nFolds', 2, ...
-            'paramName', NaN, 'paramValues', NaN, 'lossFunction', ...
-            @square_error, 'shuffle', false);
-         
-    [nTrain, ~] = size(X);
+            'paramName', NaN, 'paramValues', NaN, 'loss', ...
+            'square error', 'shuffle', false);
+
+    % Set loss function
+    if strcmp(loss, 'square error')
+        lossFunction = @square_error;
+    elseif strcmp(loss, 'zero one loss')
+        lossFunction = @zero_one_loss;
+    end
     
     % Shuffle dataset
-    randIndices = randperm(length(X));
-    X = X(randIndices, :);
-    y = y(randIndices, :);
+    if shuffle
+        randIndices = randperm(length(X));
+        X = X(randIndices, :);
+        y = y(randIndices, :);
+    end
 
-    % Get the fold indices for Xtrain and Xcv
-    foldIndices = crossvalind('Kfold', nTrain, nFolds);
-    
     % Make sure the param values are sorted
     paramValues = sort(paramValues);
     minError = inf;
@@ -51,31 +54,79 @@ function [bestModel] = matLearn_CV(X, y, options)
         validationError = 0;
         
         % Compute the accumulated score over the folds
-        for k = 1:nFolds
-            trainIndices =  foldIndices == k;
-            cvIndices =  foldIndices ~= k;
+        for fold = 1:nFolds
+            % Split dataset into training and validation sets
+            [Xtrain, ytrain, Xval, yval] = foldData(X, y, nFolds, fold);
             
             % Train model
-            trainedModel = model(X(trainIndices, :),y(trainIndices, :), subOptions);
+            trainedModel = model(Xtrain, ytrain, subOptions);
             % Predict y
-            yhat = trainedModel.predict(trainedModel, X(cvIndices, :));
+            yhat = trainedModel.predict(trainedModel, Xval);
             
             % Accumulate score
             validationError = validationError + ...
-                              lossFunction(yhat,y(cvIndices));  
+                              lossFunction(yhat, yval);  
         end
         disp(paramValues(j))
-        disp(validationError / k);
+        disp(validationError / nFolds);
         disp('OK');
         
         % Compare the minimum loss
-        if validationError / k < minError
+        if validationError / nFolds < minError
             minError = validationError;
             bestModel = trainedModel;
         end
    end
      
 end
+
+function [Xtrain, ytrain, Xval, yval] = foldData(X, y, nFolds, fold)
+    % nFolds: total number of folds
+    % fold: which fold to perform
+        
+    n = size(y,1);    
+    nPerFold = floor(n/nFolds); % excluding the last fold, which takes all remaining rows
+    
+    % Create validation set
+    valStart = (fold-1)*nPerFold + 1;
+    if (fold == nFolds)
+        valEnd = n;
+    else
+        valEnd = fold*nPerFold;
+    end
+    
+    yval = y(valStart:valEnd);
+    Xval = X(valStart:valEnd, :);
+    
+    % Training set may consist of 2 parts: the part before the validation
+    % set, and the part after.
+    if (fold == 1)
+        trainStart = valEnd + 1;
+        trainEnd = n;
+        ytrain = y(trainStart:trainEnd);
+        Xtrain = X(trainStart:trainEnd, :);
+    elseif (fold == nFolds)
+        trainStart = 1;
+        trainEnd = valStart - 1;
+        ytrain = y(trainStart:trainEnd);
+        Xtrain = X(trainStart:trainEnd, :);
+    else % expect 2 parts, because fold must be in the middle
+        trainStartA = 1;
+        trainEndA = valStart - 1;
+        trainStartB = valEnd + 1;
+        trainEndB = n;
+        
+        ytrain = [
+            y(trainStartA:trainEndA);
+            y(trainStartB:trainEndB)
+        ];
+        Xtrain = [
+            X(trainStartA:trainEndA, :);
+            X(trainStartB:trainEndB, :)
+        ];
+    end
+end
+
 function [score] = square_error(yhat, y)
     score = mean((yhat - y).^2);
 end
